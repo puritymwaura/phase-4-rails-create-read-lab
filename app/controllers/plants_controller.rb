49a9f81fe841183f69e06ee14e99bebb2effd9b2
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-09-27 11:19:53 +0300

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,20 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+  
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found" }, status: :not_found
+        end
+
+    def create
+        plant = Plant.create(params.permit(:name, :image, :price))
+        render json: plant, status: :created
+    end
+end
+end
